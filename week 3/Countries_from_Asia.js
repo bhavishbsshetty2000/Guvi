0982864d52c847436b43d3d48841f5f0cf457eaf
bhavishbsshetty2000 <59135133+bhavishbsshetty2000@users.noreply.github.com>
2020-11-23 10:53:47 +0530

    Create  Countries_from_Asia.js

---
+++ week 3/Countries_from_Asia.js
@@ -0,0 +1,27 @@
+//                                   *********************                 Html file                       *********************
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Countries in Asia</title>
+</head>
+<body>
+    <script src="script.js"></script>
+</body>
+</html>
+
+
+
+//                                   *********************                 JS file                       *********************
+var request=new XMLHttpRequest();
+request.open('GET','https://restcountries.eu/rest/v2/all',true);
+request.send();
+request.onload=function(){
+    var data=JSON.parse(this.responseText);
+    var Asian_countries=data.filter(item=>{
+        if(item.region==="Asia")
+        {
+            console.log( item.name);
+        }
+        })
+    
+}
